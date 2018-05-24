e66ebe93ae500e1a28add7f8d3df16f78af0dc99
Aljon Pineda <aljonjbpineda@gmail.com>
2018-05-23 23:31:04 -0400

    Created ActivityStore and Activity data model

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -6,8 +6,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import codeu.model.store.basic.ActivityStore;
+
 /** Servlet class for the activity feed */
 public class ActivityFeedServlet extends HttpServlet {
+
+  private ActivityStore activityStore;
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    setActivityStore(activityStore.getInstance());
+  }
+
+  void setActivityStore(ActivityStore activityStore) {
+    this.activityStore = activityStore;
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
--- src/main/java/codeu/model/store/persistence/PersistentStorageAgent.java
+++ src/main/java/codeu/model/store/persistence/PersistentStorageAgent.java
@@ -14,6 +14,7 @@
 
 package codeu.model.store.persistence;
 
+import codeu.model.data.Activity;
 import codeu.model.data.Conversation;
 import codeu.model.data.Message;
 import codeu.model.data.User;
@@ -102,5 +103,10 @@ public class PersistentStorageAgent {
   /** Write a Conversation object to the Datastore service. */
   public void writeThrough(Message message) {
     persistentDataStore.writeThrough(message);
+  }
+
+  /** Write an Activity object to the Datastore service. */
+  public void writeThrough(Activity activity) {
+    persistentDataStore.writeThrough(activity);
   }
 }
