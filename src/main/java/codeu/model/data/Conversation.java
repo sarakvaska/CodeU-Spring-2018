9a01f4bea775b57fc5f04a5f0ea8836a5b55a895
Aljon Pineda <aljonjbpineda@gmail.com>
2018-05-28 22:31:47 -0400

    Changed public fields to private fields

--- src/main/java/codeu/model/data/Conversation.java
+++ src/main/java/codeu/model/data/Conversation.java
@@ -22,10 +22,10 @@ import java.util.UUID;
  * created by a User and contain Messages.
  */
 public class Conversation {
-  public final UUID id;
-  public final UUID owner;
-  public final Instant creation;
-  public final String title;
+  private final UUID id;
+  private final UUID owner;
+  private final Instant creation;
+  private final String title;
 
   /**
    * Constructs a new Conversation.
