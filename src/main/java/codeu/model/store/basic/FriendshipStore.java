286e263a5f06fb88c4f9abc0c6cef101733074ae
Aljon Pineda <aljonjbpineda@gmail.com>
2018-06-22 18:22:02 -0400

    Added methods to update or remove existing friendships

--- src/main/java/codeu/model/store/basic/FriendshipStore.java
+++ src/main/java/codeu/model/store/basic/FriendshipStore.java
@@ -1,9 +1,11 @@
 package codeu.model.store.basic;
 
 import codeu.model.data.Friendship;
+import codeu.model.data.Friendship.Status;
 import codeu.model.data.User;
 import codeu.model.store.persistence.PersistentStorageAgent;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,9 +49,11 @@ public class FriendshipStore {
    */
   private PersistentStorageAgent persistentStorageAgent;
 
-  /** In-memory map of Friendships. Each User maps to a List of Users containing
-  the User's friends. */
-  private Map<User, List<User>> friendships;
+  /**
+   * In-memory map of Friendships. Each User maps to a List of Friendships,
+   * and each Friendship has a variable that stores one of the User's friends.
+   */
+  private Map<User, List<Friendship>> friendships;
 
   /** This class is a singleton, so its constructor is private. Call getInstance() instead. */
   private FriendshipStore(PersistentStorageAgent persistentStorageAgent) {
@@ -58,26 +62,71 @@ public class FriendshipStore {
   }
 
   /**
-   * Adds a friendship by adding the user's friend to a list that the user maps
+   * Adds a friendship by adding the user's friend to a list that a user maps
    * to in the HashMap.
    */
   public void addFriendship(Friendship friendship) {
     User user = friendship.getUser();
-    User friend = friendship.getFriend();
 
     if (!friendships.containsKey(user)) {
-      List<User> friends = new ArrayList<>();
-      friends.add(friend);
+      List<Friendship> friends = new ArrayList<>();
+      friends.add(friendship);
       friendships.put(user, friends);
     } else {
-      friendships.get(user).add(friend);
+      friendships.get(user).add(friendship);
     }
 
     persistentStorageAgent.writeThrough(friendship);
   }
 
+  /**
+   * Updates an existing friendship. This method is called when a friend request
+   * is either accepted or rejected.
+   */
+  public void updateFriendship(Friendship friendship) {
+    if (friendship.getStatus() == Status.ACCEPTED) {
+      acceptFriendship(friendship);
+    } else if (friendship.getStatus() == Status.REJECTED) {
+      rejectFriendship(friendship);
+    }
+  }
+
+  /**
+   * Updates the status of the existing one-way friendship from PENDING to ACCEPTED
+   * by first looking through the list of friends to find the friend that accepted
+   * the request.
+   *
+   * Then, a new Friendship object is made to represent the now two-way friendship
+   * both users have with each other.
+   */
+  public void acceptFriendship(Friendship friendship) {
+    for (Friendship friend : friendships.get(friendship.getUser())) {
+      if (friend.getFriend() == friendship.getFriend()) {
+        friend.setStatus(Status.ACCEPTED);
+        break;
+      }
+    }
+
+    Friendship mutualFriendship = new Friendship(
+        friendship.getFriend(), friendship.getUser(), UUID.randomUUID(),
+        Status.ACCEPTED, Instant.now()
+    );
+
+    addFriendship(mutualFriendship);
+  }
+
+  /**
+   * Removes the rejected friendship from the HashMap, then updates the friendship
+   * in Datastore.
+   */
+  public void rejectFriendship(Friendship friendship) {
+    friendships.get(friendship.getUser()).remove(friendship);
+
+    persistentStorageAgent.writeThrough(friendship);
+  }
+
   /** Access the current set of friendships known to the application. */
-  public Map<User, List<User>> getFriendships() {
+  public Map<User, List<Friendship>> getFriendships() {
     return friendships;
   }
 
@@ -85,7 +134,7 @@ public class FriendshipStore {
    * Sets the List of Friendships stored by this FriendshipStore.
    * This should only be called once, when the data is loaded from Datastore.
    */
-  public void setFriendships(Map<User, List<User>> friendships) {
+  public void setFriendships(Map<User, List<Friendship>> friendships) {
     this.friendships = friendships;
   }
 }
